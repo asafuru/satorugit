2927b10ee427d247d2bdc909c094edd43ccbd516
Satoru <asahuru11@gmail.com>
2022-07-05 12:36:55 +0900

    0705

--- satohum/js/script.js
+++ satohum/js/script.js
@@ -1,24 +1,21 @@
-const main = document.querySelectorAll(".main-menu");
-const item = Array.prototype.slice.call(main,0);
+/*
+headerにあるbuttonと
+サイドバーにあるbuttonにそれぞれjs用のクラス
+js-switchを与えておく
+js-switchのボタンをクリックしたと同時に
+is-openのクラスがそれぞれのボタンに付与され
+スイッチがオンの状態
+スイッチがオンの状態になるとis-openのクラスに記述された
+cssのスタイルが適応されてサイドバー出現
+サイドバーはZ指定してあるのでheaderは隠れる
+サイドバーのbuttonをクリックすると
+header、sidebarのis-openが取り除かれ
+sidebarが引っ込む
+*/
 
-item.forEach(function (element) {
-  element.addEventListener("mouseover",function(){
-    element.querySelector(".sub-menu").classList.add("open");
-  },false);
-  element.addEventListener("mouseout",function(){
-    element.querySelector(".sub-menu").classList.remove("open");
-  },false);
-});
-
-//ハンバーガーボタン
 $(function(){
-  $( ".js-hamburger" ).click( function() {
-    $( this ).toggleClass( "is-open" );
-    $( ".menu" ).toggleClass( "is-open" );
-    //$( "body" ).toggleClass( "open" );
+  $(".js-switch").click(function(){
+    $(".l-sidebar").toggleClass("is-open")
   });
 });
-//ボタン押したらスライドトグルで開く
-//$("button").click(function () {
-//  $(".menu").slideToggle();
-//});
+
